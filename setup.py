73606a3a161ae563e693f0ee3304eb214be13336
PanQiWei <594557445@qq.com>
2023-04-25 01:09:30 +0800

    release v0.0.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extensions = [
 setup(
     name="auto_gptq",
     packages=find_packages(),
-    version="v0.0.3-dev",
+    version="v0.0.3",
     install_requires=requirements,
     extras_require=extras_require,
     ext_modules=extensions,
