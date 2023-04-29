3b74c9758e27cc4a9262d4c447bd9897772c828c
qwopqwop200 <qwopqwop200@gmail.com>
2023-04-29 18:18:21 +0900

    change save name

--- examples/quantization/basic_usage_wikitext2.py
+++ examples/quantization/basic_usage_wikitext2.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 
 pretrained_model_dir = "facebook/opt-125m"
-quantized_model_dir = "opt-125m-4bit"
+quantized_model_dir = "opt-125m_4bit_128g"
 
 # os.makedirs(quantized_model_dir, exist_ok=True)
 def get_wikitext2(nsamples, seed, seqlen, model):
