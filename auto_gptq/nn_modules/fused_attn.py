34201dbff9b7660274d31a6315a420dc03df7968
qwopqwop200 <qwopqwop200@gmail.com>
2023-05-04 09:05:07 +0900

    support faster and model load strict

--- auto_gptq/nn_modules/fused_attn.py
+++ auto_gptq/nn_modules/fused_attn.py
@@ -79,7 +79,7 @@ class QuantLlamaAttention(nn.Module):
 
         return attn_output, attn_weights, past_key_value
         
-def make_quant_attn(model, use_triton=False, groupsize=-1, desc_act=False):
+def make_quant_attn(model, use_triton=False, groupsize=-1, use_cuda_fp16=True, desc_act=False):
     """
     Replace all LlamaAttention modules with QuantLlamaAttention modules, fusing the q, k, v projections.
     """
@@ -104,8 +104,10 @@ def make_quant_attn(model, use_triton=False, groupsize=-1, desc_act=False):
         scales = torch.cat([q_proj.scales, k_proj.scales, v_proj.scales], dim=1)
         g_idx = torch.cat([q_proj.g_idx, k_proj.g_idx, v_proj.g_idx], dim=0)
         bias = torch.cat([q_proj.bias, k_proj.bias, v_proj.bias], dim=0) if q_proj.bias is not None else None
-
-        qkv_layer = QuantLinear(q_proj.bits, q_proj.groupsize, q_proj.infeatures, q_proj.outfeatures + k_proj.outfeatures + v_proj.outfeatures, True if q_proj.bias is not None else False)
+        if not(desc_act) or groupsize == -1:
+            qkv_layer = QuantLinear(q_proj.bits, q_proj.groupsize, q_proj.infeatures, q_proj.outfeatures + k_proj.outfeatures + v_proj.outfeatures, True if q_proj.bias is not None else False, use_cuda_fp16 = use_cuda_fp16)
+        else:
+            qkv_layer = QuantLinear(q_proj.bits, q_proj.groupsize, q_proj.infeatures, q_proj.outfeatures + k_proj.outfeatures + v_proj.outfeatures, True if q_proj.bias is not None else False)
         qkv_layer.qweight = qweights
         qkv_layer.qzeros = qzeros
         qkv_layer.scales = scales
