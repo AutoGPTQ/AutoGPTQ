7af93624d7668d6c03abefe0ddca4a43d5f17932
PanQiWei <594557445@qq.com>
2023-05-28 22:30:38 +0800

    remove useless code

--- examples/peft/peft_lora_clm.py
+++ examples/peft/peft_lora_clm.py
@@ -116,7 +116,6 @@ lr_scheduler = get_linear_schedule_with_warmup(
     num_warmup_steps=0,
     num_training_steps=(len(train_dataloader) * num_epochs),
 )
-model.base_model.peft_config["default"].total_step = len(train_dataloader) * num_epochs
 
 # training and evaluation
 with torch.cuda.amp.autocast():
