172deae049a0e249ed1ce02587c08b1e306dcafd
PanQiWei <594557445@qq.com>
2023-08-09 12:03:31 +0800

    expose disable_exllama argument

--- auto_gptq/modeling/auto.py
+++ auto_gptq/modeling/auto.py
@@ -81,6 +81,7 @@ class AutoGPTQForCausalLM:
         trust_remote_code: bool = False,
         warmup_triton: bool = False,
         trainable: bool = False,
+        disable_exllama: bool = False,
         **kwargs
     ) -> BaseGPTQForCausalLM:
         model_type = check_and_get_model_type(model_name_or_path, trust_remote_code)
@@ -120,6 +121,7 @@ class AutoGPTQForCausalLM:
             trust_remote_code=trust_remote_code,
             warmup_triton=warmup_triton,
             trainable=trainable,
+            disable_exllama=disable_exllama,
             **keywords
         )
 
