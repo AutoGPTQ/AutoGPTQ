8254da4f154df4b326b2f79822a7ca65b2124481
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-08-24 17:47:14 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 common_setup_kwargs = {
-    "version": "0.4.1",
+    "version": "0.4.2",
     "name": "auto_gptq",
     "author": "PanQiWei",
     "description": "An easy-to-use LLMs quantization package with user-friendly apis, based on GPTQ algorithm.",
