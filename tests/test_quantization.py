4694e2a987b0380f42965adf34e30dc79257ce3a
fxmarty <9808326+fxmarty@users.noreply.github.com>
2023-11-07 17:52:29 +0100

    Fix quantize method with None mask (#408)
    * fix quantize method with none mask
    
    * add test

---
+++ tests/test_quantization.py
@@ -0,0 +1,31 @@
+import unittest
+import tempfile
+
+from transformers import AutoTokenizer
+
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+
+class TestQuantization(unittest.TestCase):
+    def test_quantize(self):
+        pretrained_model_dir = "saibo/llama-1B"
+
+        tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True)
+        examples = [
+            tokenizer(
+                "auto-gptq is an easy-to-use model quantization library with user-friendly apis, based on GPTQ algorithm."
+            )
+        ]
+        quantize_config = BaseQuantizeConfig(
+            bits=4,
+            group_size=128,
+            desc_act=False,
+        )
+
+        model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir,
+                                                    quantize_config=quantize_config,
+                                                    use_flash_attention_2=False)
+
+        model.quantize(examples)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            model.save_pretrained(tmpdirname)
