297788de740fadbd47351e10902b09eb83af0688
LaaZa <jesse.laatu@gmail.com>
2023-11-22 19:57:38 +0200

    Support for StableLM Epoch models.

--- auto_gptq/modeling/_const.py
+++ auto_gptq/modeling/_const.py
@@ -22,6 +22,7 @@ SUPPORTED_MODELS = [
     "internlm",
     "qwen",
     "xverse",
+    "stablelm_epoch",
 ]
 if compare_transformers_version("v4.28.0", op="ge"):
     SUPPORTED_MODELS.append("llama")
