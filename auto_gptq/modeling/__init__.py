c29a62052490543d0b2f8030a4291086f1581bb4
fxmarty <9808326+fxmarty@users.noreply.github.com>
2024-02-19 09:51:54 +0100

    add back missing import

--- auto_gptq/modeling/__init__.py
+++ auto_gptq/modeling/__init__.py
@@ -1,4 +1,4 @@
-from ._base import BaseQuantizeConfig
+from ._base import BaseGPTQForCausalLM, BaseQuantizeConfig
 from .auto import GPTQ_CAUSAL_LM_MODEL_MAP, AutoGPTQForCausalLM
 from .baichuan import BaiChuanGPTQForCausalLM
 from .bloom import BloomGPTQForCausalLM
