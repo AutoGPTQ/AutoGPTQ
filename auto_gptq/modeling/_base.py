4e694f038e0cdf9873cbf5e9de89dde713b09452
fxmarty <9808326+fxmarty@users.noreply.github.com>
2024-03-26 12:47:20 +0100

    fix logger

--- auto_gptq/modeling/_base.py
+++ auto_gptq/modeling/_base.py
@@ -69,6 +69,7 @@ logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(levelname)s - %(message)s")
 handler.setFormatter(formatter)
+logger.propagate = False
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
