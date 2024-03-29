32c3c9cb88cd0436f0bc6a157a8196d50aea505e
diegomontoya <xing@fictionpress.com>
2024-03-29 05:24:10 +0000

    re-enable router.layer

--- auto_gptq/modeling/dbrx.py
+++ auto_gptq/modeling/dbrx.py
@@ -10,6 +10,7 @@ class DbrxGPTQForCausalLM(BaseGPTQForCausalLM):
         ["norm_attn_norm.norm_2"],
         ["norm_attn_norm.attn.Wqkv"],
         ["norm_attn_norm.attn.out_proj"],
+        ["ffn.router.layer"],
         ["ffn.experts.mlp"],
     ]
 
